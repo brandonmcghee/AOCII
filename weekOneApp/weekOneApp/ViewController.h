d98f4b973ad2d9022f6b0786882983b65686614a
brandonmcghee <brandonmcghee@fullsail.edu>
2013-07-09 18:26:46 -0400

    Initial Commit
    Created Project files

---
+++ weekOneApp/weekOneApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  weekOneApp
+//
+//  Created by Brandon McGhee on 7/9/13.
+//  Copyright (c) 2013 Brandon McGhee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ weekOneApp/weekOneApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  weekOneApp
+//
+//  Created by Brandon McGhee on 7/9/13.
+//  Copyright (c) 2013 Brandon McGhee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
